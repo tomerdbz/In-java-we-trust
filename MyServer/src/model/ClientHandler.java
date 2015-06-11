5746aadc9edf6c3bf52f6a0484f7e836b583cb94
Tomerico <tomerdbz@gmail.com>
2015-06-11 19:27:43 +0300

    Arichtectural change

--- MyServer/src/model/ClientHandler.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public interface ClientHandler {
-void handleClient(InputStream inFromClient,OutputStream outToClient) throws IOException;
-}
