c954d697e42cb17ac1a456faf6d9c44835983dd8
Tomerico <tomerdbz@gmail.com>
2015-06-17 18:19:40 +0300

    Maze Properties added. just give it to ClassInputDialog and it will do all the magic.

---
+++ MVP/src/gui/MazeProperties.java
@@ -0,0 +1,46 @@
+package gui;
+
+public class MazeProperties {
+	private int rows;
+	private int cols;
+	private int rowSource;
+	private int colSource;
+	private int rowGoal;
+	private int colGoal;
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getCols() {
+		return cols;
+	}
+	public void setCols(int cols) {
+		this.cols = cols;
+	}
+	public int getRowSource() {
+		return rowSource;
+	}
+	public void setRowSource(int rowSource) {
+		this.rowSource = rowSource;
+	}
+	public int getColSource() {
+		return colSource;
+	}
+	public void setColSource(int colSource) {
+		this.colSource = colSource;
+	}
+	public int getRowGoal() {
+		return rowGoal;
+	}
+	public void setRowGoal(int rowGoal) {
+		this.rowGoal = rowGoal;
+	}
+	public int getColGoal() {
+		return colGoal;
+	}
+	public void setColGoal(int colGoal) {
+		this.colGoal = colGoal;
+	}
+}
